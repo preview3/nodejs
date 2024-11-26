36d5c902301219a7ffb4192030debd638e6d583c
Lalit Rajak <lalitrajak312@gmail.com>
2024-11-26 10:15:47 +0530

    🚑 fix: Update mongo/main.js and add mongoose connection and model for students
    - Updated the `mongo/main.js` file to include a connection to MongoDB using Mongoose and added a new file for student models in `mongo/models/students.js`.
    - Updated require statements, configured dotenv, set up express app, connected to MongoDB, and added model for students.
    - Added error handling for MongoDB connection and a route to display "Hello, World!".
    
    The changes ensure proper database connectivity and lay the foundation for managing student data within the MongoDB database.

--- mongo/main.js
+++ mongo/main.js
@@ -1,17 +1,29 @@
-  const express = require('express');
-    const bodyParser = require('body-parser');
-    const dotenv = require('dotenv');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-    dotenv.config();
+const app = express();
 
-    const app = express();
-    const port = process.env.PORT || 3000; 
-    app.use(bodyParser.json());
+mongoose
+  .connect("mongodb://localhost:27017/preview3", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((error) => {
+    console.error("Connection error:", error);
+  });
 
-    app.get('/', (req, res) => {
-    res.send('Hello, World!');
-    });
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-    app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
